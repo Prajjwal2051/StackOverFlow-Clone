be7ae097c3939574ceaa1b177d860fdb83735244
Prajjwal2051 <prajjwal2051@gmail.com>
2026-01-16 00:06:54 +0530

    style: format code for better readability in comment collection creation

--- my-app/models/server/comment.collection.ts
+++ my-app/models/server/comment.collection.ts
@@ -3,7 +3,7 @@ import { databases } from "./config"
 import { Permission } from "appwrite"
 
 export default async function createCommentCollection() {
-    await databases.createCollection(db,commentCollection,commentCollection,[
+    await databases.createCollection(db, commentCollection, commentCollection, [
         Permission.read("any"),
         Permission.read("users"),
         Permission.create("users"),
@@ -13,12 +13,12 @@ export default async function createCommentCollection() {
     console.log(`Collection ${commentCollection} created`)
     // now we will create attributes for the collection
     await Promise.all([
-        databases.createStringAttribute(db,commentCollection,"content",1000,true),
-        databases.createEnumAttribute(db,commentCollection,"type",["answer","question"],true),
-        databases.createStringAttribute(db,commentCollection,"typeId",50,true),
-        databases.createStringAttribute(db,commentCollection,"authorId",50,true),
-        databases.createDatetimeAttribute(db,commentCollection,"createdAt",true),
-        databases.createDatetimeAttribute(db,commentCollection,"updatedAt",true),
+        databases.createStringAttribute(db, commentCollection, "content", 1000, true),
+        databases.createEnumAttribute(db, commentCollection, "type", ["answer", "question"], true),
+        databases.createStringAttribute(db, commentCollection, "typeId", 50, true),
+        databases.createStringAttribute(db, commentCollection, "authorId", 50, true),
+        databases.createDatetimeAttribute(db, commentCollection, "createdAt", true),
+        databases.createDatetimeAttribute(db, commentCollection, "updatedAt", true),
     ])
 
 }
