94a227b18a8d48de1350ad9a5f5b9b5a4ccba4b2
Prajjwal2051 <prajjwal2051@gmail.com>
2026-02-09 01:14:09 +0530

    feat: implement login page with authentication handling

---
+++ my-app/(auth)/login/page.tsx
@@ -0,0 +1,42 @@
+import { useAuthStore } from "@/store/Auth";
+import React from "react";
+
+function LoginPage() {
+    const { login } = useAuthStore()
+    const [isLoading, setIsLoading] = React.useState(false)
+    const [error, setError] = React.useState("")
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        if (!email || !password) {
+            setError(() => "Please fill all fields")
+            return
+        }
+
+
+        setIsLoading(true)
+        setError(() => "")
+
+        const loginResponse = await login(email.toString(), password.toString())
+        if (loginResponse.error) {
+            setError(() => loginResponse.error!.message)
+        }
+
+        setIsLoading(false)
+
+
+    }
+
+    return (
+        <div>
+            LoginPage
+        </div>
+    )
+
+}
+
+export default LoginPage
